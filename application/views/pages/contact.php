54ea6e745d8aeace32c00cf5e214a892c95b5df5
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-12-01 19:33:35 -0600

    Updated input form fields

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -3,20 +3,34 @@
     <?= $send ?>
 </div>
 <div class="container">
-    <div class="row">
-        <form id="email-form" name="emailform" action="contact" method="post">
+    <div class="form-div">
+        <form id="email-form" name="email-form" action="contact" method="post">
             <div class="form-group">
-                <input type="text" class="form-control" id="name" name="name" placeholder="Your Name" maxlength="64" value="">
-                <input type="text" class="form-control" id="email1" name="email1" placeholder="Email" maxlength="64" value="">
-                <input type="text" class="form-control" id="email2" name="email2" placeholder="Re-Enter Email" maxlength="64" value="">
-                <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" maxlength="64" value="">
-                <textarea class="form-control" id="message" name="message" placeholder="Enter Message" maxlength="1000" rows="10"></textarea>
+                <label for="name">Name :</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Your Name" maxlength="64" value="" required>
+
+               <label for="email1">Email :</label>
+                <input type="text" class="form-control" id="email1" name="email1" placeholder="Email" maxlength="64" value="" required>
+
+
+               <label for="email2">Re-Enter Email :</label>
+                <input type="text" class="form-control" id="email2" name="email2" placeholder="Re-Enter Email" maxlength="64" value="" required>
+
+
+               <label for="subject">Subject :</label>
+                <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" maxlength="64" value="" required>
+
+
+               <label for="message">Message :</label>
+                <textarea class="form-control" cols="40" id="message" name="message" placeholder="Enter Your Message" maxlength="1000" rows="10"  required ></textarea>
             </div>
-            <div class="form-group">
-                <button id="email-send" type="button" class="">Send</button>&nbsp;&nbsp;
-                <button id="email-clear" type="button" class="">Clear</button>
+            <div class="form-actions">
+                <button id="email-send" type="submit" class="btn btn-success">Send</button>&nbsp;&nbsp;
+                <button id="email-clear" type="reset" class="btn btn-primary">Clear</button>
             </div>
         </form>
 
     </div>
 </div>
+
+
