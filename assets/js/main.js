f661a891b4be9f11232fde84660939c23876e1aa
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-12-01 19:33:58 -0600

    updated client-side validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,85 @@
-/*
- Your Header Here
- */
+/**************************************************************
+ Name: Yolanda Gunter
+ Assignment: Final Project
+ Purpose: CodeIgniter, jQuery and Boostrap
+ Notes: Pulling everything I've learned together for my Final Project
+ **************************************************************/
+
 
 "use strict";
 
-function validEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
+$(document).ready(function () {
+    $('#email-form').validate(
+        {
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                email1: {
+                    required: true,
+                    email: true
+                },
+                email2: {
+                    required: true,
+                    email: true,
+                    equalTo: "#email1"
+                },
+                subject: {
+                    required: true,
+                    minlength: 5
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                },
+            },
+            messages: {
+                name: {
+                    required: "Please enter your name",
+                    minlength: "Minimum length is 2 characters"
+                },
+                email1: {
+                    required: "Please enter a valid email",
+                },
+                email2: {
+                    required: "Please re-enter your email",
+                    equalTo: "Your emails must match"
+                },
+                subject: {
+                    required: "Please enter a subject",
+                    minlength: "Minimum length is 5 characters"
+                },
+                message: {
+                    required: "Please enter your message",
+                    minlength: "Minimum length is 20 characters"
+                },
+            },
+            highlight: function(element) {
+                $(element).closest('.control-group').removeClass('success').addClass('error');
+            },
+            success: function(element) {
+                element
+                    .text('OK!').addClass('valid')
+                    .closest('.control-group').removeClass('error').addClass('success');
+            }
+        });
 
-function clearForm() {
-    /*
-     * add your code here
-     */
-}
 
-function validate() {
-    var errorMessage = "";
-
-    /*
-     * add your code here
-     */
-
-    return errorMessage;
-}
-
-$("#email-send").click(function () {
-    //uncomment this to test back-end validation
-    /*
-     $("#email-form").submit();
-     return false;
-     */
-
-    var msg = validate();
-    console.log(msg);
-    if (msg) {
-        $("#msg").html(msg);
-    } else {
+    $("#email-send").click(function () {
+        //uncomment this to test back-end validation
         $("#email-form").submit();
-    }
-    return false;
-});
+        var msg = validate();
+        console.log(msg);
+        if (msg ) {
+            $("#msg").html(msg);
+        } else {
+            $("#email-form").submit();
+        }
+    });
 
-$("#email-clear").click(function () {
-    clearForm();
-    return false;
+    $("#email-clear").click(function () {
+        clearForm();
+    });
 });
 
