a17b0966e112b325a92a9cfac271b47fe48f58aa
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-12-04 21:10:11 -0600

    minor update to footer textual content

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -21,28 +21,24 @@
                 <div class="col-lg-4 col-md-4 center">
                     <h3>Life Goals</h3>
                         <i class="fa fa-fire"></i>
-                        <p class="goals">Keep my daughter, Victoria, encouraged!</p>
+                        <p class="goals">Keep my mother, Yolanda, encouraged!</p>
                 </div>
+             </div>
+       </div>
 
-                       </div>
-                   </div>
-
-                   <hr>
-
-                   <!-- My Copyright Below Here -->
-                   <div class="copyright">
-                       <div class="row">
-                           <div class="col-md-6 center">
-                                <p>Copyright &copy; 2019 Y. E. M. Gunter</p>
-                           </div>
-                           <div class="col-md-6 text-md-right">
-                               <a href="#about">About</a>
-                               <span>|</span>
-                               <a href="#contact">Contact</a>
-                           </div>
-                       </div>
-                   </div>
+       <hr>
+       <div class="copyright">
+           <div class="row">
+               <div class="col-md-6 center">
+                    <p>Copyright &copy; 2019 Y. E. M. Gunter</p>
                </div>
+               <div class="col-md-6 center">
+                   <a href="about">About</a>
+                   <span>|</span>
+                   <a href="contact">Contact</a>
+               </div>
+           </div>
+       </div>
 
         <!-- jQuery first then Bootstrap JS -->
         <script src="assets/js/jquery-3.4.1.min.js"></script>
@@ -50,5 +46,6 @@
 
         <!-- custom JS -->
         <script src="assets/js/main.js"></script>
-    </body>
+    </div>
+</body>
 </html>
