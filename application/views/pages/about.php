e32e9ec36f06422f381148f5a49379a798c3e5ee
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-12-07 07:25:39 -0600

    Updated About content

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,4 +1,26 @@
 
-
-
-<h1>About</h1>
+<div class=container-about>
+    <div class="intro-section">
+        <h1>Victoria "Tuki" Gunter - Founder and Owner</h1>
+    </div>
+    <!-- My About Below Here-->
+    <div class="container">
+        <div class="row">
+              <div class="col-md-6 col-xs-6 text-right">
+                  <img src="assets/images/yemgunter.jpg" class="face" alt="Owner, Victoria "Tuki" Gunter"/>
+              </div>
+              <div class="col-md-6 col-xs-6">
+                <p>S.T.E.M. Classes influenced me to create <img src="assets/images/banner-sm.png" class="logo" alt="@yemgunter small logo"/></p>
+                <h2>About Me</h2>
+                <p>I am a foodie. The science of the perfect combinations of water, natural flavors and glucose makes thirst quenching drinks.</p>
+              </div>
+        </div>
+        <div class="row">
+              <div class="col-md-12 col-xs-12">
+                <hr>
+                <p>As a natural opportunist, I combine simple accounting principles with science and started my lemonade stand.</p>
+                <p>Check out my <a href="gallery">gallery</a> page to see how I, with local venue sponsorships, brought smiles and treats to my community during SXSW 2019.</p>
+              </div>
+        </div>
+    </div>
+</div>
